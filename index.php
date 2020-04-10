eeacf4c0da89e5f16c167b2de643c2142a500ab7
dheeraj1806 <63419137+dheeraj1806@users.noreply.github.com>
2020-04-10 19:16:07 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+    require './includes/common.php';
+
+?>
+
+<html>
+    
+    <head>
+        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="./css/style.css">
+        <title>Index</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+		
+		<link rel="stylesheet" href="style.css" type="text/css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed|Rubik" rel="stylesheet">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    </head>
+    
+    <body>
+        
+        <?php
+            include './includes/header.php';           
+        ?>
+        
+        <div class="container" style="margin-top: 100px;">
+            
+        </div>
+        <div id="search-layer"></div>
+    </body>
+    
+</html>

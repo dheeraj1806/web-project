f7e3967c8cc6cb238e12e279763ea21fcdf2fb3e
dheeraj1806 <63419137+dheeraj1806@users.noreply.github.com>
2020-04-10 21:24:50 +0530

    Delete common.php

--- includes/common.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define("HOST", "localhost");
-    define("USER", "root");
-    define("PASSWORD", "");
-    define("DATABASE_NAME", "my_shooz");
-    $con = mysqli_connect(HOST, USER, PASSWORD, DATABASE_NAME) or die(mysqli_errno($con));
-    session_start();
